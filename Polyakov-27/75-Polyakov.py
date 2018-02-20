cf031965eea2674a5332962de8ec34c5c710477f
ectuser <m.ectuser@gmail.com>
2018-02-20 23:09:06 +0700

    75 add solution

--- Polyakov-27/75-Polyakov.py
+++ Polyakov-27/75-Polyakov.py
@@ -12,3 +12,12 @@ for i in range(N):
 	if a % 6 == 0:
 		k6 += 1
 		amount += N - k6
+		# либо эта строка всерху
+	elif a % 3 == 0:
+		k3 += 1
+	elif a % 2 == 0:
+		k2 += 1
+#amount += (N - k6) * k6 + (k6 * (k6 - 1)) // 2
+# либо эта срока
+amount += k3 * k2
+print(amount)
